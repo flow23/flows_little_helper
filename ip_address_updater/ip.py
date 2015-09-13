c775bd114c6407b6326afc31d343fc34d8af5898
Florian Wallburg <florian.wallburg@web.de>
2015-09-13 18:57:42 +0200

    Added logging

--- ip_address_updater/ip.py
+++ ip_address_updater/ip.py
@@ -2,58 +2,49 @@
 
 from gmail import GMail,GMailWorker,GMailHandler,Message
 import configparser
-import sys
+import sys,os
 from requests import get
 import logging
 import logging.handlers
 
+CWD = os.path.dirname(os.path.abspath(__file__))
+
 # debug(), info(), warning(), error() and critical()
 def setupLogging():
-    logging.basicConfig(filename='ip.log',level=logging.DEBUG)
+    logging.basicConfig(format='%(levelname)s %(asctime)s %(module)s.%(funcName)s()[%(lineno)d] %(message)s',
+        filename='%s/ip.log' % CWD,
+        level=logging.DEBUG)
     logger = logging.getLogger(__name__)
-
-    # File handler for /var/log/some.log
-    #serverlog = logging.FileHandler('/var/log/some.log')
-    #serverlog.setLevel(logging.DEBUG)
-    #serverlog.setFormatter(logging.Formatter(
-    #    '%(asctime)s %(pathname)s [%(process)d]: %(levelname)s %(message)s'))
-
-    # Syslog handler
-    #syslog = logging.handlers.SysLogHandler(address='/dev/log')
-    #syslog.setLevel(logging.WARNING)
-    #syslog.setFormatter(logging.Formatter(
-    #    '%(pathname)s [%(process)d]: %(levelname)s %(message)s'))
-
-    # Combined logger used elsewhere in the script
-    #logger = logging.getLogger('wbs-server-log')
-    #logger.setLevel(logging.DEBUG)
-    #logger.addHandler(serverlog)
-    #logger.addHandler(syslog)
 
     return logger
 
+def sendMail():
+    logger.info('Sending mail to %s', config['GMail']['to'])
+
+    gmail = GMail(config['GMail']['username'],config['GMail']['password'])
+    mail = Message(subject='New IP @ KA',
+        to=config['GMail']['to'],
+        sender='Automatic IP updater',
+        text='Reported from: %s\nIP: %s' % (config['General']['hostname'],ip) )
+    gmail.send(mail)
+    gmail.close()
+
 try:
     logger = setupLogging()
+
     ip = get('https://api.ipify.org').text
-    logger.info('My public IP address is %s', ip)
+    logger.info('Public IP address %s', ip)
 
     config = configparser.ConfigParser()
-    #settings._interpolation = configparser.ExtendedInterpolation()
     config.read('ip.cfg')
 
     if (config['General']['IP'] != ip):
-        logger.info('IP address change from %s to %s', ['General']['IP'], ip)
+        logger.info('IP address change from %s to %s', config['General']['IP'], ip)
         config['General']['IP'] = ip
         
-        logger.info('Sending mail')
-        gmail = GMail(config['GMail']['username'],config['GMail']['password'])
-        mail = Message(subject='New IP @ KA',
-                       to='florian.wallburg@web.de',
-                       sender='Automatic IP updater',
-                       text='Reported from: %s\nIP: %s' % (config['General']['hostname'],ip) )
-        gmail.send(mail)
-        gmail.close()
-        with open('ip.cfg', 'w') as configfile:    # save
+        sendMail()
+
+        with open('ip.cfg', 'w') as configfile:
             config.write(configfile)
     else:
         logger.info('No IP address change')
